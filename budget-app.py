6d239112c172f3920489f30f6c27be9153063eaa
david <davidtiffeneau@live.fr>
2023-01-07 12:02:41 +0100

    add file

--- budget-app.py
+++ budget-app.py
@@ -14,14 +14,18 @@ layout = "centered"
 # -----------------------------------------------------------------------------------
 
 st.set_page_config(page_title=titre, page_icon=icon, layout=layout)
-st.title(titre + " " + icon
+st.title(titre + " " + icon)
 
 #------------- VALEURS DU MENU DEROULANT POUR SELECTIONNER UNE PERIODE --------------
-annee = [datetime.today().year, datetime.today().year+1]
+annees = [datetime.today().year, datetime.today().year+1]
 mois = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"]
 # months = list(calendar.month_name[1:])
 
 #------------------------- ENTREES & SAUVEGARDE DES PERIODES ------------------------
 st.header(f"Entrée des données en {devise}")
+with st.form("Format d'entrée", clear_on_submit=True):
+    col1, col2 = st.columns(2)
+    col1.selectbox("Selectionner un mois:", mois, key="mois")
+    col2.selectbox("Selectionner une année:", annees, key="année")
 
 
